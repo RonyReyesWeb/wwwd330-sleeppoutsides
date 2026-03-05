c7824b2d857770f1288708061aae3ee14ffa8404
Rony Reyes <rony@caidci.com>
2026-03-05 14:07:55 -0800

    eliminating console in product.js due to lint warning

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,9 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs"; 
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
-console.log(dataSource.findProductById(productId));
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
